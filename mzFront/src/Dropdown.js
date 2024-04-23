767060840043a152b945b92253bd6d754da54cbd
MinWook6457 <minuk6457@gmail.com>
2024-04-23 22:38:39 +0900

    jiwoo/front branch merge

---
+++ mzFront/src/Dropdown.js
@@ -0,0 +1,12 @@
+function Dropdown() {
+
+    return (
+      <>
+        <li>naver.com</li>
+        <li>gmail.com</li>
+        <li>daum.net</li>
+      </>
+    );
+  }
+  
+  export default Dropdown;
